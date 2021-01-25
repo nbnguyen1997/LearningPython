3f2f1e355ad016cfe159de6329512b54b98220fb
nbnguyen1997 <hoctapbinhnguyen@gmail.com>
2021-01-26 02:37:09 +0700

    Chapter8/Exercise5

---
+++ Chapter8/exercise5.py
@@ -0,0 +1,24 @@
+def rotate_word(string,number):
+    numer_index = 0
+    number_step = 0
+    result=""
+    for item in string:
+        number_char = ord(item)
+        if 65 <= number_char <=90:
+            number_index = 65
+        elif 97 <= number_char <= 122:
+            number_index=97
+        else:
+            print("Chuoi chi chua cac ky tu a-z,A-Z")
+            return
+        number_char = ( number_char - number_index + number) % 26 + number_index
+        result += chr(number_char)
+    return result
+
+def function_input():
+    string = input("Nhap chuoi: ")
+    number = int(input("Nhap khoang cach xoay: "))
+
+    print(rotate_word(string=string,number=number))
+
+function_input()
