c56ad24b0801a2f99756eb45d2cf2ef77f383ca5
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-03 22:53:58 +0700

    [modified] print the number of times various classifications appear

--- Chapter18/main.py
+++ Chapter18/main.py
@@ -1,18 +1,26 @@
 from play_card import Card, Deck, Hand
 
 
-de = Deck()
-de.shuffle_card()
-num_hands = input("Enter number player: ")
+# num_hands = input("Enter number player: ")
+num_hands = 7
 
-list_hand = de.deal_hands(int(num_hands), 7)
 
-for item in list_hand:
-    # print(item["label"])
-    # for i in item["cards"]:
-    #     print("\t", i)
-    # print()
-    print(item)
-    # print("Have contains a flush: ", item.has_flush())
+hist_classify = {}
 
+for i in range(10000):
+    de = Deck()
+    de.shuffle_card()
+    list_hand = de.deal_hands(int(num_hands), 7)
+    for item in list_hand:
+        # print(item["label"])
+        # for i in item["cards"]:
+        #     print("\t", i)
+        # print()
+        hist_classify[item.classify()] = hist_classify.get(
+            item.classify(), 0) + 1
+        # print(item)
+        # print("Have contains a flush: ", item.has_flush())
+print("The number of times various classifications appear in 70000 hand:")
+for key in hist_classify:
+    print(key, hist_classify[key])
 # print(de)
