3e317ee959cafb4d753fe017c44866e0a3878a5b
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-11 22:39:30 +0700

    [create] unittest exercise3

--- Chapter5/test_exercise2.py
+++ Chapter5/test_exercise2.py
@@ -6,3 +6,7 @@ class Test_exercise2(unittest.TestCase):
     def test_check_fermat(self):
         self.assertTrue(check_fermat(3, 4, 5, 2))
         self.assertFalse(check_fermat(4, 3, 5, 5))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Chapter5/test_exercise3.py
@@ -0,0 +1,12 @@
+import unittest
+from Chapter5.exercise3 import is_triangle
+
+
+class Test_exercise3(unittest.TestCase):
+    def test_is_triangle(self):
+        self.assertTrue(is_triangle(3, 4, 5))
+        self.assertFalse(is_triangle(1, 1, -4))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
