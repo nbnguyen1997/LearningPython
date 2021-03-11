40027663c6982d4524732f89ce88219090ff125a
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-11 21:59:27 +0700

    [modified] convert return True/False

--- Chapter5/exercise3.py
+++ Chapter5/exercise3.py
@@ -1,13 +1,15 @@
 def is_triangle(a, b, c):
-    if (a > b + c ) or ( b > a + c ) or ( c > a + b ):
-        print("No")
-    else:
-        print("Yes")
+    if (a > b + c) or (b > a + c) or (c > a + b):
+        return False
+    return True
+
 
 def check_numbers():
     a = int(input("Choose a number for a: "))
     b = int(input("Choose a number for b: "))
     c = int(input("Choose a number for c: "))
-    return is_triangle(a = a , b = b, c = c)
+    return is_triangle(a=a, b=b, c=c)
 
-check_numbers()
+
+if __name__ == "__main__":
+    print(check_numbers())
