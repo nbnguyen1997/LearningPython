af512d51ca60a471bd09c2ff53400de725fba211
nbnguyen1997 <binhnguyen2114@gmail.com>
2021-03-11 18:22:51 +0700

    [modified] convert to function

--- Chapter5/exercise1.py
+++ Chapter5/exercise1.py
@@ -1,22 +1,26 @@
 import time
 
-time_days = 24*60*60
-time_hours = 60*60
-time_minutes = 60
+
+def main():
+    time_days = 24*60*60
+    time_hours = 60*60
+    time_minutes = 60
+
+    times = time.time()
+
+    days = times//time_days
+    times = times % time_days
+    hours = times//time_hours
+    times = times % time_hours
+    minutes = times // time_minutes
+    times = times - minutes*time_minutes
+
+    print(times)
+    print(int(days), end=' day, time:: ')
+    print(int(hours), end=':')
+    print(int(minutes), end='\':')
+    print(times, end='\'\':')
 
 
-times = time.time()
-
-days = times//time_days
-times = times%time_days
-hours = times//time_hours
-times = times %time_hours
-minutes = times // time_minutes
-times = times - minutes*time_minutes
-
-
-print(times)
-print(int(days),end=' day, time:: ')
-print(int(hours),end=':')
-print(int(minutes),end='\':')
-print(times,end='\'\':')
+if __name__ == "__main__":
+    main()
